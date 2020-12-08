78133d16874eeb4d68dee6df9c835dfb1a91cd68
melissikiss <melissikiss@gmail.com>
2020-12-08 12:52:07 -0800

    added GET and DELETE routes

--- server.js
+++ server.js
@@ -20,6 +20,70 @@ app.get('/', (req, res) => {
     });
 })
 
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+// request candidate by id
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates 
+                 WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+// delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+  
+      res.json({
+        message: 'successfully deleted',
+        changes: this.changes
+      });
+    });
+  });
+
+// create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+             VALUES (?, ?, ?, ?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+db.run(sql, params, function(err, result) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result, this.lastID);
+  });
+
+
 // default response for any other request(Not Found) Catch all (make sure this is the last route above the listener or else it will override all others)
 app.use((req, res) => {
     res.status(404).end();
@@ -46,3 +110,24 @@ db.on('open', () => {
 //     console.log('Conneced to the election database');
 // })
 // We created a new object, db. This instance was created with the election.db file. The callback function informs us if there's an error in the connection. 
+
+
+// db.all(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+// in this code, the db object is using the all() method. this method runs the SQL query and executes the callback with all the resulting rows that match the query. Once it is executed, 
+// the callback function captures the responses from the query in two variables: the err, which is the error response, and rowws, which is the database query response. IF there are no
+// errors, the err value is null. This method is the key component that allowd SQL commands to be written in a Node.js application. 
+
+
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result){
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result, this, this.changes);
+// });
+// The method run() will execute an SQL query but won't retrieve any result data. 
+// The question mark denotes a placeholder, making this a PREPARED STATEMENT. Prepared statements can have placeholders that can be filled in dynamically with real values at runtime. 
+// An additional param argument can provide values for prepared statement placeholders. Here, we are hardcoding 1 temporarily to demonstrate how prepared statements work. If we need 
+// additional placeholders, the param argument can be an array that hold multiple values. 
+// An ES5 function is used for the callback. This allows us to take advantage of the database object that's returned in the callback function. 
